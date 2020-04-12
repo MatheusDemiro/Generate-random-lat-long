d0050e859d988f200627d5f0ef89b0089e0a7765
MatheusDemiro <mpdemiro@hotmail.com>
2020-04-12 13:21:02 -0300

    Updating functions.

--- generate_aleatory_lat_long.py
+++ generate_aleatory_lat_long.py
@@ -1,50 +1,43 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[27]:
+# In[5]:
 
 
 import pandas as pd
 import random
 import folium
-import pycristoforo as pyc
 
-def generate_random_data(lat, lon, num_rows):
+def generate_random_data(latitude, longitude, radius, num_rows):
     points = {"lat": [], "lng": []}
     for _ in range(num_rows):
-        hex1 = '%012x' % random.randrange(16**12) # 12 char random string
-        flt = float(random.randint(0,100))
-        dec_lat = random.random()/100
-        dec_lon = random.random()/100
+        lt = random.uniform(-1, 1)*radius + latitude
+        lng = random.uniform(-1, 1)*radius + longitude
 
-        points["lat"].append(lat + dec_lat)
-        points["lng"].append(lon + dec_lon)
+        points["lat"].append(lt)
+        points["lng"].append(lng)
     return points
 
 
-# In[41]:
+# In[12]:
 
 
 center = (-8.047692, -34.887040)
 
-points = pd.DataFrame(data=generate_random_data(center[0], center[1], 100))
+points = pd.DataFrame(data=generate_random_data(center[0], center[1], 0.0002, 10))
 
 recife_antigo = folium.Map(
-    location=[points.median()[1], points.median()[0]],
+    location=[center[0], center[1]],
     zoom_start=15
 )
 
-#country = pyc.get_shape("Brazil")
 
-#points = pyc.geoloc_generation(country, 2000, "Brazil")
-
-
-# In[42]:
+# In[13]:
 
 
 for i in points.values:
     folium.Marker(
-        location=[i[1], i[0]],
+        location=[i[0], i[1]],
     ).add_to(recife_antigo)
 
 recife_antigo
